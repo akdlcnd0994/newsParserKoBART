c0c3ab43b63b20c2bf687c53c6cbb3bd14407b97
양휘준 <akdlcnd0994@naver.com>
2024-03-27 00:29:43 +0900

    db complete

--- parserKoBART.py
+++ parserKoBART.py
@@ -1,13 +1,29 @@
 import torch
 import requests
 import re
+import cx_Oracle
 from bs4 import BeautifulSoup
 from transformers import PreTrainedTokenizerFast
 from transformers import BartForConditionalGeneration
 
+cx_Oracle.init_oracle_client(lib_dir=r"C:\Program Files\Oracle\instantclient_19_9")
+connection = cx_Oracle.connect(user='admin', password='Rheodml123!!', dsn='react_high')
+cursor = connection.cursor()
+
 enterUrl = "https://m.entertain.naver.com/ranking"
 newsUrl = "https://news.naver.com/section/10"
 rankingUrl = "https://news.naver.com/main/ranking/popularDay.naver"
+
+def db(idx, title, content, img, link):
+    sql = 'insert into news(idx,title,content,img,link) values(:1,:2,:3,:4,:5)'
+    # 테이블에 데이터 삽입
+    cursor.execute(sql, (idx,title,content,img,link))
+
+    # 변경사항 commit
+    connection.commit()
+
+
+
 
 def short(text):
     tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
@@ -21,7 +37,7 @@ def short(text):
     summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=1024,  eos_token_id=1)
     temp = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
 
-    print(temp)
+    return temp
 
 def newsParser(url, idx):
     r = requests.get(url)
@@ -46,15 +62,16 @@ def newsParser(url, idx):
 
         #koBART 호출
 
-        short(text)
+        content = short(text)
 
-        #db - idx, title, content, link
+        db(idx, title, content, img, link)
+        
 
 def rankingParser(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     datas = soup.find("div", {"class" : "rankingnews_box_wrap"}).find_all("div", {"class" : "rankingnews_box"})
-
+    a=7
     for data in datas:
         img = data.find("img")["src"]
         company = data.find("strong").get_text()
@@ -74,9 +91,10 @@ def rankingParser(url):
 
         #koBART 호출
 
-        short(text)
-
-        #db - idx, title, content, link
+        content = short(text)
+        
+        db(a, title, content, img, link)
+        a+=1
         
 
 
@@ -103,13 +121,22 @@ def enterParser(url):
 
         #koBART 호출
 
-        short(text)
+        content = short(text)
 
-        #db - idx = 6, title, content, link
+        db(6, title, content, img, link)
+        
+
+sql='delete from news'
+cursor.execute(sql)
+connection.commit()
 
 
-#for i in range(0,6):
-#    newsParser("https://news.naver.com/section/10" + str(i), i)
-#enterParser(enterUrl)
+for i in range(0,6):
+    newsParser("https://news.naver.com/section/10" + str(i), i)
+enterParser(enterUrl)
 rankingParser(rankingUrl)
+
+cursor.close()
+connection.close()
+
 
